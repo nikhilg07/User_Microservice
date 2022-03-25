74b49af0ecf5bb6fd9f81020bd5473ac9dbd08fe
nikhilg07 <43913706+nikhilg07@users.noreply.github.com>
2022-03-25 12:32:54 +0530

    Update UserRepository.java

--- user_microservice/src/main/java/com/quinbay/ecommerce/user_microservice/repository/UserRepository.java
+++ user_microservice/src/main/java/com/quinbay/ecommerce/user_microservice/repository/UserRepository.java
@@ -1,10 +1,18 @@
 package com.quinbay.ecommerce.user_microservice.repository;
 
+
 import com.quinbay.ecommerce.user_microservice.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
+
+    Optional<UserEntity> findByEmailAndPassword(String email,String password);
+    Optional<UserEntity> findByEmail(String email);
+
 }
 
